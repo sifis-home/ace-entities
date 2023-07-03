533bce8fce74fb33cc020f670487a2853afa4b77
marco-rasori <marco.rasori@gmail.com>
2023-07-03 15:35:46 +0200

    Add Jacoco results and publish them on gh-pages

--- scripts/build-ace-entities.sh
+++ scripts/build-ace-entities.sh
@@ -20,8 +20,7 @@ fi
 # Run mvn on ace-entities
 # https://stackoverflow.com/questions/65092032/maven-build-failed-but-exit-code-is-still-0
 echo "*** Building and packaging ACE entities ***"
-# mvn clean org.jacoco:jacoco-maven-plugin:0.8.6:prepare-agent install org.jacoco:jacoco-maven-plugin:0.8.6:report | tee mvn_res
-mvn clean package | tee mvn_res
+mvn --batch-mode clean org.jacoco:jacoco-maven-plugin:0.8.10:prepare-agent install org.jacoco:jacoco-maven-plugin:0.8.10:report | tee mvn_res
 if grep 'BUILD FAILURE' mvn_res;then exit 1; fi;
 if grep 'BUILD SUCCESS' mvn_res;then exit 0; else exit 1; fi;
 rm mvn_res
